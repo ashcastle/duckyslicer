ashcastle/duckyslicer

commit ee240f6affb50c56ef60c6c18f73829ce47021b5

SLA backend refactored, except Hollowing

src/libslic3r/SLAPrint.hpp
@@ -313,15 +313,26 @@ private:
     public:
         sla::SupportTree::UPtr  support_tree_ptr; // the supports
         std::vector<ExPolygons> support_slices;   // sliced supports
+        TriangleMesh tree_mesh, pad_mesh, full_mesh;
         
         inline SupportData(const TriangleMesh &t)
-            : sla::SupportableMesh{t, {}, {}}
+            : sla::SupportableMesh{t.its, {}, {}}
         {}
         
         sla::SupportTree::UPtr &create_support_tree(const sla::JobController &ctl)
         {
             support_tree_ptr = sla::SupportTree::create(*this, ctl);
+            tree_mesh = TriangleMesh{support_tree_ptr->retrieve_mesh(sla::MeshType::Support)};
             return support_tree_ptr;
+        }
+
+        void create_pad(const ExPolygons &blueprint, const sla::PadConfig &pcfg)
+        {
+            if (!support_tree_ptr)
+                return;
+
+            support_tree_ptr->add_pad(blueprint, pcfg);
+            pad_mesh = TriangleMesh{support_tree_ptr->retrieve_mesh(sla::MeshType::Pad)};
         }
     };
     
@@ -569,7 +580,7 @@ sla::PadConfig::EmbedObject builtin_pad_cfg(const SLAPrintObjectConfig& c);
 
 sla::PadConfig make_pad_cfg(const SLAPrintObjectConfig& c);
 
-bool validate_pad(const TriangleMesh &pad, const sla::PadConfig &pcfg);
+bool validate_pad(const indexed_triangle_set &pad, const sla::PadConfig &pcfg);
 
 
 } // namespace Slic3r

src/admesh/stl.h
@@ -149,9 +149,11 @@ struct indexed_triangle_set
 	}
 
 	std::vector<stl_triangle_vertex_indices> 	indices;
-	std::vector<stl_vertex>       				    vertices;
+    std::vector<stl_vertex>       				    vertices;
 	//FIXME add normals once we get rid of the stl_file from TriangleMesh completely.
 	//std::vector<stl_normal> 					      normals
+
+    bool empty() const { return indices.empty() || vertices.empty(); }
 };
 
 extern bool stl_open(stl_file *stl, const char *file);

src/libslic3r/SLA/Hollowing.cpp
@@ -154,14 +154,14 @@ InteriorPtr generate_interior(const TriangleMesh &   mesh,
     return interior;
 }
 
-Contour3D DrainHole::to_mesh() const
+indexed_triangle_set DrainHole::to_mesh() const
 {
     auto r = double(radius);
     auto h = double(height);
-    sla::Contour3D hole = sla::cylinder(r, h, steps);
-    Eigen::Quaterniond q;
-    q.setFromTwoVectors(Vec3d{0., 0., 1.}, normal.cast<double>());
-    for(auto& p : hole.points) p = q * p + pos.cast<double>();
+    indexed_triangle_set hole = sla::cylinder(r, h, steps);
+    Eigen::Quaternionf q;
+    q.setFromTwoVectors(Vec3f{0.f, 0.f, 1.f}, normal);
+    for(auto& p : hole.vertices) p = q * p + pos;
     
     return hole;
 }
@@ -292,7 +292,7 @@ void cut_drainholes(std::vector<ExPolygons> & obj_slices,
 {
     TriangleMesh mesh;
     for (const sla::DrainHole &holept : holes)
-        mesh.merge(sla::to_triangle_mesh(holept.to_mesh()));
+        mesh.merge(TriangleMesh{holept.to_mesh()});
     
     if (mesh.empty()) return;
     

src/libslic3r/SLA/Hollowing.hpp
@@ -58,7 +58,7 @@ struct DrainHole
     bool get_intersections(const Vec3f& s, const Vec3f& dir,
                            std::array<std::pair<float, Vec3d>, 2>& out) const;
     
-    Contour3D to_mesh() const;
+    indexed_triangle_set to_mesh() const;
     
     template<class Archive> inline void serialize(Archive &ar)
     {

src/libslic3r/SLA/IndexedMesh.cpp
@@ -10,59 +10,75 @@
 #include <libslic3r/SLA/Hollowing.hpp>
 #endif
 
-namespace Slic3r { namespace sla {
+namespace Slic3r {
+
+namespace sla {
 
 class IndexedMesh::AABBImpl {
 private:
     AABBTreeIndirect::Tree3f m_tree;
 
 public:
-    void init(const TriangleMesh& tm)
+    void init(const indexed_triangle_set &its)
     {
         m_tree = AABBTreeIndirect::build_aabb_tree_over_indexed_triangle_set(
-            tm.its.vertices, tm.its.indices);
+            its.vertices, its.indices);
     }
 
-    void intersect_ray(const TriangleMesh& tm,
-                       const Vec3d& s, const Vec3d& dir, igl::Hit& hit)
+    void intersect_ray(const indexed_triangle_set &its,
+                       const Vec3d &               s,
+                       const Vec3d &               dir,
+                       igl::Hit &                  hit)
     {
-        AABBTreeIndirect::intersect_ray_first_hit(tm.its.vertices,
-                                                  tm.its.indices,
-                                                  m_tree,
-                                                  s, dir, hit);
+        AABBTreeIndirect::intersect_ray_first_hit(its.vertices, its.indices,
+                                                  m_tree, s, dir, hit);
     }
 
-    void intersect_ray(const TriangleMesh& tm,
-                       const Vec3d& s, const Vec3d& dir, std::vector<igl::Hit>& hits)
+    void intersect_ray(const indexed_triangle_set &its,
+                       const Vec3d &               s,
+                       const Vec3d &               dir,
+                       std::vector<igl::Hit> &     hits)
     {
-        AABBTreeIndirect::intersect_ray_all_hits(tm.its.vertices,
-                                                 tm.its.indices,
-                                                 m_tree,
-                                                 s, dir, hits);
+        AABBTreeIndirect::intersect_ray_all_hits(its.vertices, its.indices,
+                                                 m_tree, s, dir, hits);
     }
 
-    double squared_distance(const TriangleMesh& tm,
-                            const Vec3d& point, int& i, Eigen::Matrix<double, 1, 3>& closest) {
+    double squared_distance(const indexed_triangle_set & its,
+                            const Vec3d &                point,
+                            int &                        i,
+                            Eigen::Matrix<double, 1, 3> &closest)
+    {
         size_t idx_unsigned = 0;
-        Vec3d closest_vec3d(closest);
-        double dist = AABBTreeIndirect::squared_distance_to_indexed_triangle_set(
-            tm.its.vertices,
-            tm.its.indices,
-            m_tree, point, idx_unsigned, closest_vec3d);
-        i = int(idx_unsigned);
+        Vec3d  closest_vec3d(closest);
+        double dist =
+            AABBTreeIndirect::squared_distance_to_indexed_triangle_set(
+                its.vertices, its.indices, m_tree, point, idx_unsigned,
+                closest_vec3d);
+        i       = int(idx_unsigned);
         closest = closest_vec3d;
         return dist;
     }
 };
 
-IndexedMesh::IndexedMesh(const TriangleMesh& tmesh)
-    : m_aabb(new AABBImpl()), m_tm(&tmesh)
+template<class M> void IndexedMesh::init(const M &mesh)
 {
-    auto&& bb = tmesh.bounding_box();
+    BoundingBoxf3 bb = bounding_box(mesh);
     m_ground_level += bb.min(Z);
 
     // Build the AABB accelaration tree
-    m_aabb->init(tmesh);
+    m_aabb->init(*m_tm);
+}
+
+IndexedMesh::IndexedMesh(const indexed_triangle_set& tmesh)
+    : m_aabb(new AABBImpl()), m_tm(&tmesh)
+{
+    init(tmesh);
+}
+
+IndexedMesh::IndexedMesh(const TriangleMesh &mesh)
+    : m_aabb(new AABBImpl()), m_tm(&mesh.its)
+{
+    init(mesh);
 }
 
 IndexedMesh::~IndexedMesh() {}
@@ -87,34 +103,34 @@ IndexedMesh::IndexedMesh(IndexedMesh &&other) = default;
 
 const std::vector<Vec3f>& IndexedMesh::vertices() const
 {
-    return m_tm->its.vertices;
+    return m_tm->vertices;
 }
 
 
 
 const std::vector<Vec3i>& IndexedMesh::indices()  const
 {
-    return m_tm->its.indices;
+    return m_tm->indices;
 }
 
 
 
 const Vec3f& IndexedMesh::vertices(size_t idx) const
 {
-    return m_tm->its.vertices[idx];
+    return m_tm->vertices[idx];
 }
 
 
 
 const Vec3i& IndexedMesh::indices(size_t idx) const
 {
-    return m_tm->its.indices[idx];
+    return m_tm->indices[idx];
 }
 
 
-
 Vec3d IndexedMesh::normal_by_face_id(int face_id) const {
-    return m_tm->stl.facet_start[face_id].normal.cast<double>();
+
+    return its_unnormalized_normal(*m_tm, face_id).cast<double>().normalized();
 }
 
 

src/libslic3r/SLA/IndexedMesh.hpp
@@ -15,6 +15,8 @@
   #include "libslic3r/SLA/Hollowing.hpp"
 #endif
 
+struct indexed_triangle_set;
+
 namespace Slic3r {
 
 class TriangleMesh;
@@ -29,7 +31,7 @@ using PointSet = Eigen::MatrixXd;
 class IndexedMesh {
     class AABBImpl;
     
-    const TriangleMesh* m_tm;
+    const indexed_triangle_set* m_tm;
     double m_ground_level = 0, m_gnd_offset = 0;
     
     std::unique_ptr<AABBImpl> m_aabb;
@@ -40,9 +42,12 @@ class IndexedMesh {
     std::vector<DrainHole> m_holes;
 #endif
 
+    template<class M> void init(const M &mesh);
+
 public:
     
-    explicit IndexedMesh(const TriangleMesh&);
+    explicit IndexedMesh(const indexed_triangle_set&);
+    explicit IndexedMesh(const TriangleMesh &mesh);
     
     IndexedMesh(const IndexedMesh& other);
     IndexedMesh& operator=(const IndexedMesh&);
@@ -130,7 +135,7 @@ public:
 
     Vec3d normal_by_face_id(int face_id) const;
 
-    const TriangleMesh * get_triangle_mesh() const { return m_tm; }
+    const indexed_triangle_set * get_triangle_mesh() const { return m_tm; }
 };
 
 // Calculate the normals for the selected points (from 'points' set) on the

src/libslic3r/SLA/Pad.cpp
@@ -1,7 +1,7 @@
 #include <libslic3r/SLA/Pad.hpp>
 #include <libslic3r/SLA/SpatIndex.hpp>
 #include <libslic3r/SLA/BoostAdapter.hpp>
-#include <libslic3r/SLA/Contour3D.hpp>
+//#include <libslic3r/SLA/Contour3D.hpp>
 #include <libslic3r/TriangleMeshSlicer.hpp>
 
 #include "ConcaveHull.hpp"
@@ -29,25 +29,23 @@ namespace Slic3r { namespace sla {
 
 namespace {
 
-Contour3D walls(
+indexed_triangle_set walls(
     const Polygon &lower,
     const Polygon &upper,
     double         lower_z_mm,
     double         upper_z_mm)
 {
-    Wall w = triangulate_wall(lower, upper, lower_z_mm, upper_z_mm);
-
-    Contour3D ret;
-    ret.points = std::move(w.first);
-    ret.faces3 = std::move(w.second);
+    indexed_triangle_set w;
+    triangulate_wall(w.vertices, w.indices, lower, upper, lower_z_mm,
+                     upper_z_mm);
     
-    return ret;
+    return w;
 }
 
 // Same as walls() but with identical higher and lower polygons.
-Contour3D inline straight_walls(const Polygon &plate,
-                                double         lo_z,
-                                double         hi_z)
+inline indexed_triangle_set straight_walls(const Polygon &plate,
+                                           double         lo_z,
+                                           double         hi_z)
 {
     return walls(plate, plate, lo_z, hi_z);
 }
@@ -357,8 +355,10 @@ ExPolygon offset_contour_only(const ExPolygon &poly, coord_t delta, Args...args)
     return std::move(tmp2.front());
 }
 
-bool add_cavity(Contour3D &pad, ExPolygon &top_poly, const PadConfig3D &cfg,
-                ThrowOnCancel thr)
+bool add_cavity(indexed_triangle_set &pad,
+                ExPolygon &           top_poly,
+                const PadConfig3D &   cfg,
+                ThrowOnCancel         thr)
 {
     auto logerr = []{BOOST_LOG_TRIVIAL(error)<<"Could not create pad cavity";};
 
@@ -377,18 +377,18 @@ bool add_cavity(Contour3D &pad, ExPolygon &top_poly, const PadConfig3D &cfg,
     top_poly = pdiff.front();
 
     double z_min = -cfg.wing_height, z_max = 0;
-    pad.merge(walls(inner_base.contour, middle_base.contour, z_min, z_max));
+    its_merge(pad, walls(inner_base.contour, middle_base.contour, z_min, z_max));
     thr();
-    pad.merge(triangulate_expolygon_3d(inner_base, z_min, NORMALS_UP));
+    its_merge(pad, triangulate_expolygon_3d(inner_base, z_min, NORMALS_UP));
 
     return true;
 }
 
-Contour3D create_outer_pad_geometry(const ExPolygons & skeleton,
-                                    const PadConfig3D &cfg,
-                                    ThrowOnCancel      thr)
+indexed_triangle_set create_outer_pad_geometry(const ExPolygons & skeleton,
+                                               const PadConfig3D &cfg,
+                                               ThrowOnCancel      thr)
 {
-    Contour3D ret;
+    indexed_triangle_set ret;
 
     for (const ExPolygon &pad_part : skeleton) {
         ExPolygon top_poly{pad_part};
@@ -399,45 +399,45 @@ Contour3D create_outer_pad_geometry(const ExPolygons & skeleton,
         thr();
         
         double z_min = -cfg.height, z_max = 0;
-        ret.merge(walls(top_poly.contour, bottom_poly.contour, z_max, z_min));
+        its_merge(ret, walls(top_poly.contour, bottom_poly.contour, z_max, z_min));
 
         if (cfg.wing_height > 0. && add_cavity(ret, top_poly, cfg, thr))
             z_max = -cfg.wing_height;
 
         for (auto &h : bottom_poly.holes)
-            ret.merge(straight_walls(h, z_max, z_min));
+            its_merge(ret, straight_walls(h, z_max, z_min));
         
-        ret.merge(triangulate_expolygon_3d(bottom_poly, z_min, NORMALS_DOWN));
-        ret.merge(triangulate_expolygon_3d(top_poly, NORMALS_UP));
+        its_merge(ret, triangulate_expolygon_3d(bottom_poly, z_min, NORMALS_DOWN));
+        its_merge(ret, triangulate_expolygon_3d(top_poly, NORMALS_UP));
     }
 
     return ret;
 }
 
-Contour3D create_inner_pad_geometry(const ExPolygons & skeleton,
-                                    const PadConfig3D &cfg,
-                                    ThrowOnCancel      thr)
+indexed_triangle_set create_inner_pad_geometry(const ExPolygons & skeleton,
+                                               const PadConfig3D &cfg,
+                                               ThrowOnCancel      thr)
 {
-    Contour3D ret;
+    indexed_triangle_set ret;
 
     double z_max = 0., z_min = -cfg.height;
     for (const ExPolygon &pad_part : skeleton) {
         thr();
-        ret.merge(straight_walls(pad_part.contour, z_max, z_min));
+        its_merge(ret, straight_walls(pad_part.contour, z_max, z_min));
 
         for (auto &h : pad_part.holes)
-            ret.merge(straight_walls(h, z_max, z_min));
+            its_merge(ret, straight_walls(h, z_max, z_min));
     
-        ret.merge(triangulate_expolygon_3d(pad_part, z_min, NORMALS_DOWN));
-        ret.merge(triangulate_expolygon_3d(pad_part, z_max, NORMALS_UP));
+        its_merge(ret, triangulate_expolygon_3d(pad_part, z_min, NORMALS_DOWN));
+        its_merge(ret, triangulate_expolygon_3d(pad_part, z_max, NORMALS_UP));
     }
 
     return ret;
 }
 
-Contour3D create_pad_geometry(const PadSkeleton &skelet,
-                              const PadConfig &  cfg,
-                              ThrowOnCancel      thr)
+indexed_triangle_set create_pad_geometry(const PadSkeleton &skelet,
+                                         const PadConfig &  cfg,
+                                         ThrowOnCancel      thr)
 {
 #ifndef NDEBUG
     SVG svg("pad_skeleton.svg");
@@ -447,14 +447,16 @@ Contour3D create_pad_geometry(const PadSkeleton &skelet,
 #endif
 
     PadConfig3D cfg3d(cfg);
-    return create_outer_pad_geometry(skelet.outer, cfg3d, thr)
-        .merge(create_inner_pad_geometry(skelet.inner, cfg3d, thr));
+    auto pg = create_outer_pad_geometry(skelet.outer, cfg3d, thr);
+    its_merge(pg, create_inner_pad_geometry(skelet.inner, cfg3d, thr));
+
+    return pg;
 }
 
-Contour3D create_pad_geometry(const ExPolygons &supp_bp,
-                              const ExPolygons &model_bp,
-                              const PadConfig & cfg,
-                              ThrowOnCancel thr)
+indexed_triangle_set create_pad_geometry(const ExPolygons &supp_bp,
+                                         const ExPolygons &model_bp,
+                                         const PadConfig & cfg,
+                                         ThrowOnCancel     thr)
 {
     PadSkeleton skelet;
 
@@ -471,15 +473,15 @@ Contour3D create_pad_geometry(const ExPolygons &supp_bp,
 
 } // namespace
 
-void pad_blueprint(const TriangleMesh &      mesh,
-                   ExPolygons &              output,
-                   const std::vector<float> &heights,
-                   ThrowOnCancel             thrfn)
+void pad_blueprint(const indexed_triangle_set &mesh,
+                   ExPolygons &                output,
+                   const std::vector<float> &  heights,
+                   ThrowOnCancel               thrfn)
 {
     if (mesh.empty()) return;
 
     assert(mesh.has_shared_vertices());
-    std::vector<ExPolygons> out = slice_mesh_ex(mesh.its, heights, thrfn);
+    std::vector<ExPolygons> out = slice_mesh_ex(mesh, heights, thrfn);
 
     size_t count = 0;
     for(auto& o : out) count += o.size();
@@ -500,26 +502,26 @@ void pad_blueprint(const TriangleMesh &      mesh,
     }
 }
 
-void pad_blueprint(const TriangleMesh &mesh,
-                   ExPolygons &        output,
-                   float               h,
-                   float               layerh,
-                   ThrowOnCancel       thrfn)
+void pad_blueprint(const indexed_triangle_set &mesh,
+                   ExPolygons &                output,
+                   float                       h,
+                   float                       layerh,
+                   ThrowOnCancel               thrfn)
 {
-    float gnd = float(mesh.bounding_box().min(Z));
+    float gnd = float(bounding_box(mesh).min(Z));
 
     std::vector<float> slicegrid = grid(gnd, gnd + h, layerh);
     pad_blueprint(mesh, output, slicegrid, thrfn);
 }
 
-void create_pad(const ExPolygons &sup_blueprint,
-                const ExPolygons &model_blueprint,
-                TriangleMesh &    out,
-                const PadConfig & cfg,
-                ThrowOnCancel thr)
+void create_pad(const ExPolygons &    sup_blueprint,
+                const ExPolygons &    model_blueprint,
+                indexed_triangle_set &out,
+                const PadConfig &     cfg,
+                ThrowOnCancel         thr)
 {
-    Contour3D t = create_pad_geometry(sup_blueprint, model_blueprint, cfg, thr);
-    out.merge(to_triangle_mesh(std::move(t)));
+    auto t = create_pad_geometry(sup_blueprint, model_blueprint, cfg, thr);
+    its_merge(out, t);
 }
 
 std::string PadConfig::validate() const

src/libslic3r/SLA/Pad.hpp
@@ -6,6 +6,8 @@
 #include <cmath>
 #include <string>
 
+struct indexed_triangle_set;
+
 namespace Slic3r {
 
 class ExPolygon;
@@ -13,25 +15,23 @@ class Polygon;
 using ExPolygons = std::vector<ExPolygon>;
 using Polygons = std::vector<Polygon>;
 
-class TriangleMesh;
-
 namespace sla {
 
 using ThrowOnCancel = std::function<void(void)>;
 
 /// Calculate the polygon representing the silhouette.
 void pad_blueprint(
-    const TriangleMesh &mesh,       // input mesh
+    const indexed_triangle_set &mesh,       // input mesh
     ExPolygons &        output,     // Output will be merged with
     const std::vector<float> &,     // Exact Z levels to sample
     ThrowOnCancel thrfn = [] {}); // Function that throws if cancel was requested
 
 void pad_blueprint(
-    const TriangleMesh &mesh,
-    ExPolygons &        output,
-    float               samplingheight = 0.1f,  // The height range to sample
-    float               layerheight    = 0.05f, // The sampling height
-    ThrowOnCancel       thrfn = [] {});
+    const indexed_triangle_set &mesh,
+    ExPolygons &                output,
+    float         samplingheight = 0.1f,  // The height range to sample
+    float         layerheight    = 0.05f, // The sampling height
+    ThrowOnCancel thrfn          = [] {});
 
 struct PadConfig {
     double wall_thickness_mm = 1.;
@@ -82,11 +82,12 @@ struct PadConfig {
     std::string validate() const;
 };
 
-void create_pad(const ExPolygons &support_contours,
-                const ExPolygons &model_contours,
-                TriangleMesh &    output_mesh,
-                const PadConfig & = PadConfig(),
-                ThrowOnCancel throw_on_cancel = []{});
+void create_pad(
+    const ExPolygons &    support_contours,
+    const ExPolygons &    model_contours,
+    indexed_triangle_set &output_mesh,
+    const PadConfig &             = PadConfig(),
+    ThrowOnCancel throw_on_cancel = [] {});
 
 } // namespace sla
 } // namespace Slic3r

commit 2f06510eb8fcd47c828e56abffd9c717b03dd2ea

Fix of clipping plane for SLA supports, probably broken since ee240f6

src/libslic3r/SLAPrint.hpp
@@ -323,6 +323,7 @@ private:
         {
             support_tree_ptr = sla::SupportTree::create(*this, ctl);
             tree_mesh = TriangleMesh{support_tree_ptr->retrieve_mesh(sla::MeshType::Support)};
+            tree_mesh.require_shared_vertices();
             return support_tree_ptr;
         }
 