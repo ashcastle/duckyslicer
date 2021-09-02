ashcastle/duckyslicer

commit 74a2911816f7898a2f89c49dfad05014745a217d

Faster and hopefully more reliable projection of paint-on support blockers and enforcers on a sliced mesh.

src/libslic3r/PrintObject.cpp
@@ -2294,9 +2294,13 @@ void PrintObject::project_and_append_custom_facets(
                     ? mv->seam_facets.get_facets_strict(*mv, type)
                     : mv->supported_facets.get_facets_strict(*mv, type);
             if (! custom_facets.indices.empty())
+#if 0
                 project_triangles_to_slabs(this->layers(), custom_facets, 
                     (this->trafo_centered() * mv->get_matrix()).cast<float>(),
                     seam, out);
+#else
+                slice_mesh_slabs(custom_facets, zs_from_layers(this->layers()), this->trafo_centered() * mv->get_matrix(), nullptr, &out, [](){});
+#endif
         }
 }
 

commit 99c4d032763e0f4722389a38f460ef010896081d

Fix painted seam on vertical surfaces (broken since 74a2911)

src/libslic3r/PrintObject.cpp
@@ -2294,13 +2294,12 @@ void PrintObject::project_and_append_custom_facets(
                     ? mv->seam_facets.get_facets_strict(*mv, type)
                     : mv->supported_facets.get_facets_strict(*mv, type);
             if (! custom_facets.indices.empty())
-#if 0
-                project_triangles_to_slabs(this->layers(), custom_facets, 
-                    (this->trafo_centered() * mv->get_matrix()).cast<float>(),
-                    seam, out);
-#else
-                slice_mesh_slabs(custom_facets, zs_from_layers(this->layers()), this->trafo_centered() * mv->get_matrix(), nullptr, &out, [](){});
-#endif
+                if (seam)
+                    project_triangles_to_slabs(this->layers(), custom_facets,
+                        (this->trafo_centered() * mv->get_matrix()).cast<float>(),
+                        seam, out);
+                else
+                    slice_mesh_slabs(custom_facets, zs_from_layers(this->layers()), this->trafo_centered() * mv->get_matrix(), nullptr, &out, [](){});
         }
 }
 